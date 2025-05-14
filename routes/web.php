08e15ac804256a5a7e11ec277ac8bf383ec9c1d0
jlaurentia <161397439+jlaurentia@users.noreply.github.com>
2025-05-14 16:33:19 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::get('/forgot-password', [AuthController::class, 'showForgotPassword'])->n
 Route::post('/forgot-password', [AuthController::class, 'processForgotPassword'])->name('password.update');
 
 // Customer Routes
-Route::middleware(['auth', 'customer'])->group(function () {
+//Route::middleware(['auth', 'customer'])->group(function () {
     Route::get('/', [HomeController::class, 'index'])->name('home');
     Route::get('/products', [ProductController::class, 'index'])->name('products');
     Route::get('/products/{id}', [ProductController::class, 'show'])->name('product.detail');
@@ -32,7 +32,7 @@ Route::middleware(['auth', 'customer'])->group(function () {
     Route::get('/wishlist', [CartController::class, 'wishlist'])->name('wishlist');
     Route::get('/checkout', [OrderController::class, 'checkout'])->name('checkout');
     Route::get('/orders/{id}', [OrderController::class, 'show'])->name('order.detail');
-});
+//});
 
 // Admin Routes
 Route::prefix('admin')->middleware(['auth', 'admin'])->group(function () {
