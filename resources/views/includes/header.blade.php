b3950b2e81b5c14387f08077e09770163d7c0304
amandamich <amandamdarwis@gmail.com>
2025-05-15 14:22:08 +0700

    logout cust

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -83,7 +83,7 @@
                 <li class="nav-item">
                     <a class="nav-link px-3" href="{{ route('about') }}">About Us</a>
                 </li>
-                    <form action="{{ route('logout') }}" method="POST" class="d-inline">
+                    <form action="{{ route('logout') }}" class="d-inline">
                         @csrf
                         <button type="submit" class="btn btn-link nav-link">Logout</button>
                     </form>
